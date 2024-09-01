958d8995883f88ca3d782c0ae895fe70346470b1
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-01 18:01:29 +0300

    feat: add solution and package.json

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('Hello version 0.0.0')
+
+const operationObj = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+};
+
+function mathOperation(operation, a, b) {
+    return(operationObj[operation](b, a))
+}
+
+function transformPolishNotation(str) {
+    let stack = [];
+
+    str.split(' ').forEach((item) => {
+        stack.push(item in operationObj 
+            ? mathOperation(item, stack.pop(), stack.pop())
+            : Number(item))
+    });
+
+    return stack.pop();
+};
+
+module.export = transformPolishNotation
